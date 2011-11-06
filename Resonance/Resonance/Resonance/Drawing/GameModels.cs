bc78dde4199a35b648512169797fc9a9d1eaedfe
phyros@gmail.com <phyros@gmail.com@746476e4-9158-5aec-6506-eda85f564993>
2011-11-06 23:32:00 +0000

    Changed the way Drawing loads model files, now looks them up in separate GameModels object. Moved Bender to the right a bit. Added GameModel, GameModels, DrawingInterface.

---
+++ Resonance/Resonance/Resonance/Drawing/GameModels.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace Resonance
+{
+    class GameModels
+    {
+        public static readonly int TREE = 1;
+        public static readonly int HOUSE = 2;
+        public static readonly int BAD_VIBE = 3;
+        public static readonly int GOOD_VIBE = 4;
+        public static readonly int GROUND = 5;
+        public static readonly int MUSHROOM = 6;
+
+        private GameModel Tree;
+        private GameModel Bad_vibe;
+        private GameModel Good_vibe;
+        private GameModel Ground;
+        private GameModel Mushroom;
+
+        private ContentManager Content;
+
+        /// <summary>
+        /// Creates a new GameModels object and stores all the GameModel objects in one place
+        /// to be grabbed by the Drawing class
+        /// </summary>
+        /// <param name="Content">Pass it the content manager to load textures</param>
+        public GameModels(ContentManager newContent)
+        {
+            Content = newContent;
+        }
+
+        /// <summary>
+        /// Load all the models for the game
+        /// </summary>
+        public void Load()
+        {
+            Tree = new GameModel(Content, "Drawing/Models/basicTree", 0.5f);
+            Ground = new GameModel(Content, "Drawing/Models/ground", 1f);
+            Good_vibe = new GameModel(Content, "Drawing/Models/box", 1f);
+            Bad_vibe = new GameModel(Content, "Drawing/Models/box", 1f);
+            Mushroom = new GameModel(Content, "Drawing/Models/bender", 1f);
+        }
+
+        /// <summary>
+        /// Returns a GameModel object which contains the Model and scale information
+        /// </summary>
+        /// <param name="name">Pass it the name of the model e.g GameModels.TREE</param>
+        public GameModel getModel(int name)
+        {
+            if (name == TREE) return Tree;
+            if (name == GOOD_VIBE) return Good_vibe;
+            if (name == BAD_VIBE) return Bad_vibe;
+            if (name == GROUND) return Ground;
+            if (name == MUSHROOM) return Mushroom;
+            return null;
+        }
+    }
+}
