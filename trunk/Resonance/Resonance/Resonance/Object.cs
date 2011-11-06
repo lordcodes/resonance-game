bb11447ee4da1879717b89125b888ffd1dca1344
pdt_2@hotmail.com <pdt_2@hotmail.com@746476e4-9158-5aec-6506-eda85f564993>
2011-11-06 19:39:34 +0000

    Added Object class with basic get methods for world coordinates Cords are returned as int's.

---
+++ trunk/Resonance/Resonance/Resonance/Object.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Resonance
+{
+    class Object
+    {
+        private int xWorldCord;
+        private int yWorldCord;
+        private int zWorldCord;
+
+        public int getXWorldCord()
+        {
+            return xWorldCord;
+        }
+
+        public int getZWorldCord()
+        {
+            return zWorldCord;
+        }
+        
+        public int getYWorldCord()
+        {
+            return yWorldCord;
+        }
+    }
+        
+}
