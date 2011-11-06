694c8a5057831c6f1077709cb865fd3da8ff582c
pdt_2@hotmail.com <pdt_2@hotmail.com@746476e4-9158-5aec-6506-eda85f564993>
2011-11-06 20:03:19 +0000

    Added Set methods for Object.cs

--- Resonance/Resonance/Resonance/Object.cs
+++ Resonance/Resonance/Resonance/Object.cs
@@ -11,6 +11,35 @@ namespace Resonance
         private int yWorldCord;
         private int zWorldCord;
 
+        public Object()
+        {
+            xWorldCord = 0;
+            yWorldCord = 0;
+            zWorldCord = 0;
+        }
+
+        public Object(int x, int y, int z)
+        {
+            xWorldCord = x;
+            yWorldCord = y;
+            zWorldCord = z;
+        }
+
+        public void setXWoldCord(int x)
+        {
+            xWorldCord = x;
+        }
+
+        public void setYWorldCord(int y)
+        {
+            yWorldCord = y;
+        }
+
+        public void setZWorldCord(int z)
+        {
+            zWorldCord = z;
+        }
+        
         public int getXWorldCord()
         {
             return xWorldCord;
