cf4521e9c5303cee51ab67a5b73bbd0f46a3f8a6
pdt_2@hotmail.com <pdt_2@hotmail.com@746476e4-9158-5aec-6506-eda85f564993>
2011-11-06 20:17:03 +0000

    Commented out broaken lines in Music Handler.cs

--- Resonance/Resonance/Resonance/Music/MusicHandler.cs
+++ Resonance/Resonance/Resonance/Music/MusicHandler.cs
@@ -15,7 +15,7 @@ namespace Resonance
     class MusicHandler
     {
         MusicTrack bgMusic;
-        SongTiming timing;
+        //SongTiming timing; // This line was causing compile errors so i comented it out <Phil>
         long startTime;
 
         public MusicHandler(ContentManager newContent)
