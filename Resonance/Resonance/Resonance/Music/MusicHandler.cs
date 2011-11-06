b7e4fed4fc8c1fac5062892b5da161ff7ce0ab12
Andrew Lord <andrew@lordcodes.com>
2011-11-06 15:25:45 +0000

    Added media player, pressing space plays, p pauses and s stops background music.

--- Resonance/Resonance/Resonance/Music/MusicHandler.cs
+++ Resonance/Resonance/Resonance/Music/MusicHandler.cs
@@ -14,5 +14,49 @@ namespace Resonance
 {
     class MusicHandler
     {
+        ContentManager content;
+        Song song;
+        PlayState state;
+
+        enum PlayState { PLAYING, PAUSED, STOPPED };
+
+        public MusicHandler(ContentManager newContent) 
+        {
+            content = newContent;
+            song = content.Load<Song>("Music/song");
+            state = PlayState.STOPPED;
+        }
+
+        public void playTrack()
+        {
+            if (state == PlayState.STOPPED)
+            {
+                state = PlayState.PLAYING;
+                MediaPlayer.Play(song);
+            }
+            else if (state == PlayState.PAUSED)
+            {
+                state = PlayState.PLAYING;
+                MediaPlayer.Resume();
+            }
+        }
+
+        public void stopTrack()
+        {
+            if (state == PlayState.PLAYING || state == PlayState.PAUSED)
+            {
+                state = PlayState.STOPPED;
+                MediaPlayer.Stop();
+            }
+        }
+
+        public void pauseTrack()
+        {
+            if (state == PlayState.PLAYING)
+            {
+                state = PlayState.PAUSED;
+                MediaPlayer.Pause();
+            }
+        }
     }
 }
