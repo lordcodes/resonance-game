5f1e868db03a5c7e57e172ce19ce8b20c9417059
mihaionemes@gmail.com <mihaionemes@gmail.com@746476e4-9158-5aec-6506-eda85f564993>
2011-11-09 21:35:46 +0000

    Created the world class and made some adjustments to the object class adding a new parameter called identifier which is used to store the object in the hashmap(dictionary)

--- Resonance/Resonance/Resonance/Drawing/World.cs
+++ Resonance/Resonance/Resonance/Drawing/World.cs
@@ -8,23 +8,50 @@ namespace Resonance.Drawing
 {
     class World
     {
-        private Dictionary<String, Object> objects = new Dictionary<string, Object>();
+        private Dictionary<string, Object> objects = new Dictionary<string, Object>();
 
         //I was thinking maybe every object could have like a string identifier so that I can store them into a 
         // hash map (dictionary) making updating, and deleting much more faster than just a simple link list
-        // - I didn`t update the object class because I didn`t want to change someone else`s code without talking to them
-        //first. 
+
         public void addObject(Object obj)
         {
-
+            if(checkPosition(obj.getXWorldCord(),obj.getYWorldCord(),obj.getZWorldCord(),obj.returnIdentifier()) == true)
+                objects.Add(obj.returnIdentifier() , obj);
         }
+       
+        //checks if there is another object that has the same position on the map
+        private bool checkPosition(float x, float y, float z, string ID)
+        {
+            foreach(string key in objects.Keys)
+                if(objects[key].getXWorldCord() == x && objects[key].getYWorldCord() == y && objects[key].getZWorldCord() == z && key.Equals(ID) == false)
+                     return false;
+            return true;
+        }
+
+        //removes the object from the dictionary
         public void removeObject(Object obj)
         {
-
+            objects.Remove(obj.returnIdentifier());
         }
+
+
+        //basic edge detection - checks if the new position assigned to the object does not collide with 
+        //the position of other objects.
+
         public bool updatePosition(float posX, float posY, float posZ, Object obj)
         {
-
+            if (objects[obj.returnIdentifier()] == null)
+                return false;
+            else
+                if (checkPosition(posX, posY, posZ, obj.returnIdentifier()) == true)
+                {
+                    obj.setXWoldCord(posX);
+                    obj.setYWorldCord(posY);
+                    obj.setZWorldCord(posZ);
+                    removeObject(obj);
+                    objects.Add(obj.returnIdentifier(), obj);
+                    return true;
+                }
             return false;
         }
 
