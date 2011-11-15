7bb3bbc8af70d038c93d400f1f26d5c65f86655b
alord90@googlemail.com <alord90@googlemail.com@746476e4-9158-5aec-6506-eda85f564993>
2011-11-15 14:56:53 +0000

    Good vibe added to space. It doesn't move yet.

--- trunk/Resonance/Resonance/Resonance/Drawing/GameModels.cs
+++ trunk/Resonance/Resonance/Resonance/Drawing/GameModels.cs
@@ -44,7 +44,7 @@ namespace Resonance
         public void Load()
         {
             Tree = new GameModel(Content, "Drawing/Models/basicTree", 0.5f);
-            Ground = new GameModel(Content, "Drawing/Models/ground", 1f);
+            Ground = new GameModel(Content, "Drawing/Models/terrain", 1f);
             Good_vibe = new GameModel(Content, "Drawing/Models/box", 1f);
             Bad_vibe = new GameModel(Content, "Drawing/Models/box", 1f);
             Mushroom = new GameModel(Content, "Drawing/Models/bender", 1f);
