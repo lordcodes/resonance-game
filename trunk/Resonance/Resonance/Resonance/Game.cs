fd9ec0dc3d2b88fd6541003e4fd7197052d8b784
mihaionemes@gmail.com <mihaionemes@gmail.com@746476e4-9158-5aec-6506-eda85f564993>
2011-11-19 20:23:47 +0000

    Working version

--- trunk/Resonance/Resonance/Resonance/Game.cs
+++ trunk/Resonance/Resonance/Resonance/Game.cs
@@ -9,7 +9,6 @@ using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Media;
 using BEPUphysics;
-using ResonanceLibrary;
 
 namespace Resonance
 {
