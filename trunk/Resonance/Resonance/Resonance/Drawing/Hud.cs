d67915a0ad82f98ca8e8fd730c5811e38d4fe964
phyros@gmail.com <phyros@gmail.com@746476e4-9158-5aec-6506-eda85f564993>
2011-11-20 22:57:11 +0000

    Start basic logic for the radar on the HUD

--- trunk/Resonance/Resonance/Resonance/Drawing/Hud.cs
+++ trunk/Resonance/Resonance/Resonance/Drawing/Hud.cs
@@ -17,6 +17,7 @@ namespace Resonance
         private static SpriteFont font;
         private static GraphicsDeviceManager graphics;
         private static ContentManager Content;
+        private static Dictionary<string, Vector2> dictionary = new Dictionary<string, Vector2>();
 
         public Hud(ContentManager newContent, GraphicsDeviceManager newGraphics)
         {
@@ -35,8 +36,32 @@ namespace Resonance
             spriteBatch.Begin();
             spriteBatch.DrawString(font, text, coords, Color.White, 0f, Vector2.Zero, 1f, SpriteEffects.None, 0f);
             spriteBatch.End();
+            resetGraphics();
+        }
+
+        public void Draw()
+        {
+            /*spriteBatch.Begin();
+            foreach (KeyValuePair<string, Vector2> pair in dictionary)
+            {
+                spriteBatch.DrawString(font, "BV", pair.Value, Color.White, 0f, Vector2.Zero, 1f, SpriteEffects.None, 0f);
+            }
+            spriteBatch.End();
+            resetGraphics();*/
+        }
+
+        public void updateEnemyRadar(string name, Vector3 pos)
+        {
+            Vector2 newpos = new Vector2(500 + pos.X , 200+ pos.Z);
+            if (dictionary.ContainsKey(name)) dictionary[name] = newpos;
+            else dictionary.Add(name, newpos);
+        }
+
+        private void resetGraphics()
+        {
             graphics.GraphicsDevice.BlendState = BlendState.Opaque;
             graphics.GraphicsDevice.DepthStencilState = DepthStencilState.Default;
         }
+
     }
 }
