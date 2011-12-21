621b2ba2c788ffa70316136043ac082247ca660a
alord90@googlemail.com <alord90@googlemail.com@746476e4-9158-5aec-6506-eda85f564993>
2011-12-21 12:06:30 +0000

    Started path-finding. Implemented a minheap.

--- trunk/Resonance/Resonance/Resonance/AI/Minheap.cs
+++ trunk/Resonance/Resonance/Resonance/AI/Minheap.cs
@@ -3,9 +3,42 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace Resonance.AI
+namespace Resonance
 {
     class Minheap
     {
+        private Node head;
+
+        public Minheap() { }
+
+        public void add(Node node)
+        {
+            if (head == null) head = node;
+            else if (head.After == null && node.Cost <= head.Cost)
+            {
+                node.NextElement = head;
+                head = node;
+            }
+            else
+            {
+                Node newHead = head;
+                while (newHead.NextElement != null && newHead.NextElement.Cost < node.Cost) newHead = newHead.NextElement;
+                node.NextElement = newHead.NextElement;
+                newHead.NextElement = node;
+            }
+        }
+
+        public bool hasNext()
+        {
+            if (head != null) return true;
+            else return false;
+        }
+
+        public Node extractHead()
+        {
+            Node value = head;
+            head = head.NextElement;
+            return value;
+        }
     }
 }
