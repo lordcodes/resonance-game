0148a18b5aa71e54fe930cf350114e00e8d4208c
Andrew Lord <andrew@lordcodes.com>
2011-12-22 12:37:51 +0000

    Re-doing the min heap to add better functionality.

--- Resonance/Resonance/Resonance/AI/Minheap.cs
+++ Resonance/Resonance/Resonance/AI/Minheap.cs
@@ -7,38 +7,135 @@ namespace Resonance
 {
     class Minheap
     {
-        private Node head;
+        private List<Node> list;
+        private int count;
 
-        public Minheap() { }
-
-        public void add(Node node)
+        public Minheap()
         {
-            if (head == null) head = node;
-            else if (head.After == null && node.Cost <= head.Cost)
+            list = new List<Node>();
+            count = 0;
+        }
+
+        public int Count
+        {
+            get
             {
-                node.NextElement = head;
-                head = node;
+                return count;
             }
+        }
+
+        /// <summary>
+        /// Extracts the root from the heap
+        /// </summary>
+        /// <returns>null if error, else the root node</returns>
+        public Node extractRoot()
+        {
+            if (count == 0) return null;
             else
             {
-                Node newHead = head;
-                while (newHead.NextElement != null && newHead.NextElement.Cost < node.Cost) newHead = newHead.NextElement;
-                node.NextElement = newHead.NextElement;
-                newHead.NextElement = node;
+                Node rootNode = list[0];
+                switchElements(0, count - 1);
+                count--;
+
+                return rootNode;
             }
         }
 
-        public bool hasNext()
+        /// <summary>
+        /// Peek at the root of the heap
+        /// </summary>
+        /// <returns>null if error, else the root node</returns>
+        public Node accessRoot()
         {
-            if (head != null) return true;
-            else return false;
+            if (count == 0) return null;
+            else return list[0];
         }
 
-        public Node extractHead()
+        /// <summary>
+        /// Add a node to the heap
+        /// </summary>
+        /// <param name="node">The node to add</param>
+        public void add(Node node)
         {
-            Node value = head;
-            head = head.NextElement;
-            return value;
+            if (count >= list.Count) list.Add(node);
+            else list[count] = node;
+            count++;
+        }
+
+        //PRIVATE METHODS
+
+        /// <summary>
+        /// Re-sort the heap
+        /// </summary>
+        private void heapify()
+        {
+            for (int i = parentIndex(count - 1); i >= 0; i--)
+            {
+                heapDown(i);
+            }
+        }
+
+        private void heapUp(int index)
+        {
+            Node elem = list[index];
+            while (true)
+            {
+                int parentInd = parentIndex(index);
+                break;
+            }
+        }
+
+        private void heapDown(int index)
+        {
+            while (true)
+            {
+                break;
+            }
+        }
+
+        /// <summary>
+        /// Returns the index of the parent
+        /// </summary>
+        /// <param name="index">the index you are at</param>
+        /// <returns>int - index of parent</returns>
+        private int parentIndex(int index)
+        {
+            if (count == 0) return -1;
+            else return (index - 1) / 2;
+        }
+
+        /// <summary>
+        /// Returns the index of the left child
+        /// </summary>
+        /// <param name="index">the index you are at</param>
+        /// <returns>int - index of left child</returns>
+        private int lChildIndex(int index)
+        {
+            if (count == 0) return -1;
+            else return 2 * index + 1;
+        }
+
+        /// <summary>
+        /// Returns the index of the right child
+        /// </summary>
+        /// <param name="index">the index you are at</param>
+        /// <returns>int - index of right child</returns>
+        private int rChildIndex(int index)
+        {
+            if (count == 0) return -1;
+            else return 2 * index + 2;
+        }
+
+        /// <summary>
+        /// Swap elements around in the heap
+        /// </summary>
+        /// <param name="first">first element</param>
+        /// <param name="second">second element</param>
+        private void switchElements(int first, int second)
+        {
+            Node temp = list[first];
+            list[first] = list[second];
+            list[second] = temp;
         }
     }
 }
