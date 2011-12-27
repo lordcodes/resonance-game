94cd4071366339f2191e45c295ecb699bc19155b
phyros@gmail.com <phyros@gmail.com@746476e4-9158-5aec-6506-eda85f564993>
2011-12-27 16:05:01 +0000

    Remove superfluous texture. Change game back to full screen, I left it windowed.

--- Resonance/Resonance/Resonance/Game.cs
+++ Resonance/Resonance/Resonance/Game.cs
@@ -52,10 +52,10 @@ namespace Resonance
             IsMouseVisible = false;
             IsFixedTimeStep = true;
             graphics.SynchronizeWithVerticalRetrace = true;
-            //graphics.IsFullScreen = true;
+            graphics.IsFullScreen = true;
             graphics.PreferMultiSampling = true;
-            //graphics.PreferredBackBufferWidth = 1920;
-            //graphics.PreferredBackBufferHeight = 1080;
+            graphics.PreferredBackBufferWidth = 1920;
+            graphics.PreferredBackBufferHeight = 1080;
             Window.AllowUserResizing = true;
         }
 
