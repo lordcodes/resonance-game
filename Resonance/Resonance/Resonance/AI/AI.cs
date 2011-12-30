4460f9554bd46e393ed0a83aabfa3f2175f675e7
Andrew Lord <andrew@lordcodes.com>
2011-12-30 14:47:02 +0000

    Added in ThreadRun class to run a method of your choosing in a different thread.

---
+++ Resonance/Resonance/Resonance/AI/AI.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace Resonance
+{
+    class AI
+    {
+        public AI(BadVibe enemy, Vector3 target)
+        {
+            PathFind pathFind = new PathFind();
+            ThreadRun.run(delegate { pathFind.find(enemy.Body.Position, target); }, enemy.returnIdentifier() + " path-finding.");
+        }
+
+
+    }
+}
