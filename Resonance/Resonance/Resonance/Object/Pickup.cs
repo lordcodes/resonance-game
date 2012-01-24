39842d0ca1d52c4cdc66cc34753bc7b52045bf21
alexsheppard11@gmail.com <alexsheppard11@gmail.com@746476e4-9158-5aec-6506-eda85f564993>
2012-01-24 17:45:47 +0000

    Added Pickup class

---
+++ Resonance/Resonance/Resonance/Object/Pickup.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BEPUphysics;
+using BEPUphysics.Collidables;
+using Microsoft.Xna.Framework;
+using BEPUphysics.DataStructures;
+using BEPUphysics.MathExtensions;
+using BEPUphysics.Entities;
+
+namespace Resonance
+{
+    class Pickup : StaticObject
+    {
+        //for reference
+        //public static readonly int NITROUS = 0;
+        //public static readonly int SHIELD = 1;
+        //public static readonly int FREEZE = 2;
+
+        public int powerupType;
+        private int timeToLive;
+
+        public Pickup(int modelNum, String name, Vector3 pos, int power, int time)
+            : base(modelNum, name, pos)
+        {
+            powerupType = power;
+            timeToLive = time;
+        }
+
+        public int TimeToLive
+        {
+            get
+            {
+                return timeToLive;
+            }
+
+            set
+            {
+                timeToLive = value;
+            }
+        }
+    }
+}
