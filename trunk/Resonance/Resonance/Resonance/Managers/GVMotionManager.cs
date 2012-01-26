2256497cd0106e41b735768ca22df8c924b617b0
tompico1024 <tompico1024@746476e4-9158-5aec-6506-eda85f564993>
2012-01-26 13:46:54 +0000

    Moving using motors works! Strafing works using < and > on the keyboard.

--- trunk/Resonance/Resonance/Resonance/Managers/GVMotionManager.cs
+++ trunk/Resonance/Resonance/Resonance/Managers/GVMotionManager.cs
@@ -13,17 +13,17 @@ namespace Resonance {
     /// Handles and provides an interface between button presses and Good Vibe motion.
     /// </summary>
     class GVMotionManager {
-        public static float MAX_R_SPEED                     =   0.25f;
-        public static float MAX_X_SPEED                     =   4.00f;
+        public static float MAX_R_SPEED                     =   0.20f;
+        public static float MAX_X_SPEED                     =  10.00f;
         public static float MAX_Z_SPEED                     =  12.00f;
         public static readonly float DEFAULT_MAX_Z_SPEED    =  12.00f;
-        public static float R_ACCELERATION                  =   0.01f;
-        public static float X_ACCELERATION                  =   0.25f;
+        public static float R_ACCELERATION                  =   0.02f;
+        public static float X_ACCELERATION                  =   0.50f;
         public static float Z_ACCELERATION                  =   0.50f;
         public static readonly float DEFAULT_Z_ACCELERATION =   0.50f;
         public static float R_SPEED                         =   0.00f;
-        public static float X_SPEED          = 0.00f;
-        public static float Z_SPEED          = 0.00f;
+        public static float X_SPEED                         =   0.00f;
+        public static float Z_SPEED                         =   0.00f;
 
         private static bool MOVING_F         = false;
         private static bool MOVING_B         = false;
@@ -59,18 +59,22 @@ namespace Resonance {
             Program.game.World.addToSpace(servo);
 
 
-            // ADD LINEARAXISMOTOR TO GV
-            lamZ = new LinearAxisMotor();
-
+            // ADD LINEARAXISMOTORs TO GV
+            lamZ               = new LinearAxisMotor();
             lamZ.Settings.Mode = MotorMode.VelocityMotor;
-
-            //lamZ.Settings.VelocityMotor.Softness = 0f;
-
-            lamZ.ConnectionA = gv.Body;
-            lamZ.ConnectionB = null;
-            lamZ.IsActive = true;
-
+            lamZ.ConnectionA   = null;
+            lamZ.ConnectionB   = gv.Body;
+            lamZ.Axis          = new Vector3(0f, 0f, 1f);
+            lamZ.IsActive      = true;
             Program.game.World.addToSpace(lamZ);
+
+            lamX               = new LinearAxisMotor();
+            lamX.Settings.Mode = MotorMode.VelocityMotor;
+            lamX.ConnectionA   = null;
+            lamX.ConnectionB   = gv.Body;
+            lamX.Axis          = new Vector3(0f, 0f, 1f);
+            lamX.IsActive      = true;
+            Program.game.World.addToSpace(lamX);
 
             // INITIALISATION COMPLETE
             initialised = true;
@@ -99,6 +103,8 @@ namespace Resonance {
             Quaternion eAng = Quaternion.Concatenate(cAng, dAng);
 
             servo.Settings.Servo.Goal = eAng;
+
+            //DebugDisplay.update("ROTATION", (DynamicObject.QuaternionToEuler(gv.Body.Orientation)).Y.ToString());
         }
 
         public static void boost()
@@ -109,7 +115,7 @@ namespace Resonance {
         }
 
         private static void move(float power) {
-            float inc = -power * Z_ACCELERATION;
+            float inc = power * Z_ACCELERATION;
 
             float posInc = inc;
             float posSpd = Z_SPEED;
@@ -118,7 +124,7 @@ namespace Resonance {
             if (posInc < 0) posInc *= -1;
             if (posSpd < 0) posSpd *= -1;
             if (max < 0) max *= -1;
-            if (max > MAX_R_SPEED) max = MAX_Z_SPEED;
+            if (max > MAX_Z_SPEED) max = MAX_Z_SPEED;
             if (posSpd + posInc < max) Z_SPEED += inc;
 
             Vector3 oVector = DynamicObject.QuaternionToEuler(gv.Body.Orientation);
@@ -126,34 +132,46 @@ namespace Resonance {
 
             //////////// USING VELOCITIES /////////////////
 
-            float xInc = (float)(-power * Z_ACCELERATION * Math.Sin(oVector.Y));
+            /*float xInc = (float)(-power * Z_ACCELERATION * Math.Sin(oVector.Y));
             float zInc = (float)(-power * Z_ACCELERATION * Math.Cos(oVector.Y));
 
             if (vel.X < MAX_Z_SPEED && vel.X > -MAX_Z_SPEED) vel.X += xInc;
             if (vel.Z < MAX_Z_SPEED && vel.Z > -MAX_Z_SPEED) vel.Z += zInc;
 
-            gv.Body.LinearVelocity = vel;
+            gv.Body.LinearVelocity = vel;*/
 
             ///////////// USING LINEARAXISMOTOR ///////////////
 
-            //Vector2 vf = new Vector2(0f, 1f);
-            //Vector2 v2 = MiniMap.rotateVector2(vf, oVector.Y);
-            //lamZ.Axis = new Vector3(oVector.X, oVector.Y, oVector.Z);
-            //lamZ.Axis = new Vector3(1f, 0f, 0f);
-            //lamZ.Settings.VelocityMotor.GoalVelocity = Z_SPEED;
+            lamZ.Axis = gv.Body.OrientationMatrix.Forward;
+            lamZ.Settings.VelocityMotor.GoalVelocity = Z_SPEED;
         }
 
         private static void strafe(float power) {
+            float inc = power * X_ACCELERATION;
+
+            float posInc = inc;
+            float posSpd = X_SPEED;
+            float max = power * MAX_X_SPEED;
+
+            if (posInc < 0) posInc *= -1;
+            if (posSpd < 0) posSpd *= -1;
+            if (max < 0) max *= -1;
+            if (max > MAX_X_SPEED) max = MAX_X_SPEED;
+            if (posSpd + posInc < max) X_SPEED += inc;
+
             Vector3 oVector = DynamicObject.QuaternionToEuler(gv.Body.Orientation);
             Vector3 vel     = gv.Body.LinearVelocity;
 
-            float xInc = (float)(-power * X_ACCELERATION * Math.Cos(oVector.Y));
+            /*float xInc = (float)(-power * X_ACCELERATION * Math.Cos(oVector.Y));
             float zInc = (float)(-power * X_ACCELERATION * Math.Sin(oVector.Y));
 
             if (vel.X < MAX_X_SPEED && vel.X > -MAX_X_SPEED) vel.X += xInc;
             if (vel.Z < MAX_X_SPEED && vel.Z > -MAX_X_SPEED) vel.Z += zInc;
 
-            gv.Body.LinearVelocity = vel;
+            gv.Body.LinearVelocity = vel;*/
+
+            lamX.Axis = gv.Body.OrientationMatrix.Left;
+            lamX.Settings.VelocityMotor.GoalVelocity = X_SPEED;
         }
 
 
@@ -222,6 +240,7 @@ namespace Resonance {
                 if (Z_SPEED > 0) if (Z_ACCELERATION > Z_SPEED)  Z_SPEED = 0f; else Z_SPEED -= Z_ACCELERATION;
                 if (Z_SPEED < 0) if (Z_ACCELERATION > -Z_SPEED) Z_SPEED = 0f; else Z_SPEED += Z_ACCELERATION;
                 move(0f);
+                //lamZ.Settings.VelocityMotor.GoalVelocity = 0f;
             }
 
             // Strafe based on keyboard.
@@ -231,6 +250,10 @@ namespace Resonance {
                 } else {
                     strafe(-1f);
                 }
+            } else {
+                if (X_SPEED > 0) if (X_ACCELERATION > X_SPEED)  X_SPEED = 0f; else X_SPEED -= X_ACCELERATION;
+                if (X_SPEED < 0) if (X_ACCELERATION > -X_SPEED) X_SPEED = 0f; else X_SPEED += X_ACCELERATION;
+                strafe(0f);
             }
 
             // Move / strafe based on analogue sticks (if no movement performed above).
