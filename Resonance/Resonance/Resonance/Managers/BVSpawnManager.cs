5c0cf4cc3c34b8efd72a28142e4c448dffd44c68
pdt_2@hotmail.com <pdt_2@hotmail.com@746476e4-9158-5aec-6506-eda85f564993>
2012-02-03 23:10:18 +0000

    creating bv spawner

--- Resonance/Resonance/Resonance/Managers/BVSpawnManager.cs
+++ Resonance/Resonance/Resonance/Managers/BVSpawnManager.cs
@@ -12,11 +12,28 @@ namespace Resonance
 
         private int numBVs;
         private int totalNumBVs;
+        private int spawnerCount;
+        List<BVSpawner> spawners;
 
         public BVSpawnManager() 
         {
+            spawnerCount = 2;
+            spawners = new List<BVSpawner>();
             numBVs = 0;
             totalNumBVs = 0;
+            addNewSpawner(12,12,3);
+        }
+
+        public void addNewSpawner(int totalBv, int radious, int totalActive )
+        {
+            Random r = new Random((int)DateTime.Now.Ticks);
+            int x = r.Next((int)-World.MAP_X, (int)World.MAP_X);
+            int z = r.Next((int)-World.MAP_Z, (int)World.MAP_Z);
+            Vector3 pos = new Vector3((float)x, 0.5f, (float)z);
+            BVSpawner spawn = new BVSpawner(GameModels.BV_SPAWNER, "BV_SPAWNER" + spawnerCount, pos, totalActive,radious,totalActive);
+            Program.game.World.addObject(spawn);
+            //spawners.Add(spawn);
+            spawnerCount++;
         }
 
         public void vibeDied()
@@ -26,7 +43,9 @@ namespace Resonance
 
         public void update()
         {
-            if (numBVs < MIN_BVS)
+
+
+           /* if (numBVs < MIN_BVS)
             {
                 bool placed = false;
 
@@ -63,7 +82,7 @@ namespace Resonance
                         }
                     }
                 }
-            }
+            }*/
         }
     }
 }
