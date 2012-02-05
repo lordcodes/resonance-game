aeb62a2a33517966f2545836cefdd1b2cb40a11c
phyros@gmail.com <phyros@gmail.com@746476e4-9158-5aec-6506-eda85f564993>
2012-02-05 17:50:57 +0000

    Remove some drawing debug info

--- Resonance/Resonance/Resonance/Drawing/Graphics/Shaders/ParticleShader.cs
+++ Resonance/Resonance/Resonance/Drawing/Graphics/Shaders/ParticleShader.cs
@@ -14,7 +14,6 @@ namespace Resonance
         public void setColour(Color colour)
         {
             Vector4 vc = colour.ToVector4();
-            DebugDisplay.update("colour", vc+"");
             float[] c = { vc.X * vc.W, vc.Y * vc.W, vc.Z * vc.W, vc.W };
             Effect.Parameters["Colour"].SetValue(c);
         }
