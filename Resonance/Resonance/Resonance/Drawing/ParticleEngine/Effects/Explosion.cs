51fcc52a66c70c7f63522456ed665dfece262790
tompico1024@googlemail.com <tompico1024@googlemail.com@746476e4-9158-5aec-6506-eda85f564993>
2012-02-05 20:34:00 +0000

    BVs now explode in White & Black.

--- Resonance/Resonance/Resonance/Drawing/ParticleEngine/Effects/Explosion.cs
+++ Resonance/Resonance/Resonance/Drawing/ParticleEngine/Effects/Explosion.cs
@@ -13,6 +13,31 @@ namespace Resonance {
             particlesLeft = 500;
             maxParticleSpd = 0.6f;
             maxParticleLife = 30;
+            iColour = Color.White;
+        }
+
+        protected override void generateParticles() {
+            if (particlesLeft > 0) {
+                for (int i = 0; i < emissionsPerUpdate; ++i) {
+                    Vector3 iDir = new Vector3((float)gen.NextDouble(), (float)gen.NextDouble(), (float)gen.NextDouble());
+                    if (gen.Next() % 2 == 0) iDir.X *= -1;
+                    if (gen.Next() % 2 == 0) iDir.Y *= -1;
+                    if (gen.Next() % 2 == 0) iDir.Z *= -1;
+                    iDir.Normalize();
+
+                    float iSpd  = (float) gen.NextDouble() * maxParticleSpd;
+                    int   iLife = gen.Next(maxParticleLife);
+
+                    if (gen.Next() % 4 == 0) { 
+                        particles.Add(new Particle(pos, iDir, iSpd, iPSize, iLife, iColour, 1f, Vector3.Zero));
+                    } else {
+                        particles.Add(new Particle(pos, iDir, iSpd, iPSize, iLife, Color.Black, 1f, Vector3.Zero));
+                    }
+                    particlesLeft--;
+
+                    if (particlesLeft <= 0) break;
+                }
+            }
         }
     }
 }
