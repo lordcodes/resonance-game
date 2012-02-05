8fe7f11ec51005187254477056d70900ac3dcf29
phyros@gmail.com <phyros@gmail.com@746476e4-9158-5aec-6506-eda85f564993>
2012-02-05 11:56:39 +0000

    More refactoring and optimising of drawing code ready for multiple shaders

--- trunk/Resonance/Resonance/Resonance/Drawing/Graphics/Shaders/Shader.cs
+++ trunk/Resonance/Resonance/Resonance/Drawing/Graphics/Shaders/Shader.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
 
 namespace Resonance
 {
@@ -42,6 +43,31 @@ namespace Resonance
         public void applyPass(int pass)
         {
             customEffect.CurrentTechnique.Passes[0].Apply();
+        }
+
+        public void sceneSetup(Matrix view, Matrix projection, Vector3 camera)
+        {
+            customEffect.Parameters["View"].SetValue(view);
+            customEffect.Parameters["Projection"].SetValue(projection);
+            customEffect.Parameters["CameraPosition"].SetValue(camera);
+        }
+
+        public void sceneSetup(Matrix world, Matrix view, Matrix projection, Vector3 camera)
+        {
+            customEffect.Parameters["World"].SetValue(world);
+            sceneSetup(view, projection, camera);
+        }
+
+        public void sceneSetup(Matrix world, Matrix view, Matrix projection, Vector3 camera, Texture2D texture)
+        {
+            customEffect.Parameters["ColorTexture"].SetValue(texture);
+            sceneSetup(world, view, projection, camera);
+        }
+
+        public void sceneSetup(Matrix view, Matrix projection, Vector3 camera, Texture2D texture)
+        {
+            customEffect.Parameters["ColorTexture"].SetValue(texture);
+            sceneSetup(view, projection, camera);
         }
 
         public Shader(string file)
