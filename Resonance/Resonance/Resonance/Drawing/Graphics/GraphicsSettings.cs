0d6a2c6c74651adf8f4dd8af06be92779c3723cc
phyros@gmail.com <phyros@gmail.com@746476e4-9158-5aec-6506-eda85f564993>
2012-02-12 19:43:58 +0000

    Cleaning up drawing code

---
+++ Resonance/Resonance/Resonance/Drawing/Graphics/GraphicsSettings.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Resonance
+{
+    class GraphicsSettings
+    {
+        /// Max texture size for XBOX is 4096.
+
+        /// <summary>
+        /// Resolution of the reflection texture of the floor reflections.
+        /// </summary>
+        public static int REFLECTION_TEXTURE_SIZE = 2048;
+    }
+}
