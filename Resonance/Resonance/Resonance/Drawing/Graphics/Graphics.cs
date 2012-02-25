b25a5a40128bb83f39e3bd683bea40ff81c44a17
phyros@gmail.com <phyros@gmail.com@746476e4-9158-5aec-6506-eda85f564993>
2012-02-25 18:51:59 +0000

    Fix bug that stopped texture animations from showing up

--- Resonance/Resonance/Resonance/Drawing/Graphics/Graphics.cs
+++ Resonance/Resonance/Resonance/Drawing/Graphics/Graphics.cs
@@ -242,7 +242,7 @@ namespace Resonance
                 graphics.GraphicsDevice.SetVertexBuffer(particleVertexBuffer);
             }
 
-            //graphics.GraphicsDevice.RasterizerState = RasterizerState.CullNone;
+            graphics.GraphicsDevice.RasterizerState = RasterizerState.CullNone;
 
             foreach (EffectPass pass in shader.Passes)
             {
@@ -250,7 +250,7 @@ namespace Resonance
                 graphics.GraphicsDevice.DrawUserIndexedPrimitives<VertexPositionNormalTexture>(PrimitiveType.TriangleList,particleVertices,0,4,particleIndices,0,2);
             }
 
-            //graphics.GraphicsDevice.RasterizerState = RasterizerState.CullCounterClockwise;
+            graphics.GraphicsDevice.RasterizerState = RasterizerState.CullCounterClockwise;
         }
 
 
