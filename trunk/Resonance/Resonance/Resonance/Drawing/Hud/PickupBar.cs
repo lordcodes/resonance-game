be9aab82cac28b6a98bfb82f2e8e351d63913b55
alexsheppard11@gmail.com <alexsheppard11@gmail.com@746476e4-9158-5aec-6506-eda85f564993>
2012-05-05 15:26:16 +0000

    Added pickup bars. Alpha overlay needs aligning in the left to right direction.

---
+++ trunk/Resonance/Resonance/Resonance/Drawing/Hud/PickupBar.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Resonance
+{
+     abstract class PickupBar
+    {
+        private GraphicsDeviceManager graphics;
+        private Texture2D HudTextureBare;//empty bar with border
+        private Texture2D HudTextureFull;//full health gradient
+        private Texture2D HudAlpha;//sliding overlay to mask health gradient
+        private RenderTarget2D HudBuffer;
+
+        public PickupBar(GraphicsDeviceManager graphics)
+        {
+            this.graphics = graphics;
+        }
+
+        public abstract void loadTextures(ContentManager content);
+        public abstract void saveTexture(SpriteBatch spriteBatch, int value);
+        public abstract void draw(SpriteBatch spriteBatch);
+
+        public GraphicsDeviceManager Graphics
+        {
+            get { return graphics; }
+            set { graphics = value; }
+        }
+
+        public Texture2D HUDTextureBare
+        {
+            get { return HudTextureBare; }
+            set { HudTextureBare = value; }
+        }
+
+        public Texture2D HUDTextureFull
+        {
+            get { return HudTextureFull; }
+            set { HudTextureFull = value; }
+        }
+
+        public Texture2D HUDAlpha
+        {
+            get { return HudAlpha; }
+            set { HudAlpha = value; }
+        }
+
+        public RenderTarget2D HUDBuffer
+        {
+            get { return HudBuffer; }
+            set { HudBuffer = value; }
+        }
+    }
+}
---
+++ trunk/Resonance/Resonance/Resonance/Drawing/Hud/ShieldBar.cs
@@ -0,0 +1,58 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Resonance
+{
+    class ShieldBar : PickupBar
+    {
+        public ShieldBar(GraphicsDeviceManager graphics)
+            :base(graphics)
+        {
+        }
+
+        public override void loadTextures(ContentManager content)
+        {
+            HUDTextureBare = content.Load<Texture2D>("Drawing/HUD/Textures/pickupemptyx"); //empty bar with border
+            HUDTextureFull = content.Load<Texture2D>("Drawing/HUD/Textures/pickupfullx"); //full health gradient
+            HUDAlpha = content.Load<Texture2D>("Drawing/HUD/Textures/pickupalphay"); //sliding overlay to mask health gradient
+
+            HUDBuffer = new RenderTarget2D(Graphics.GraphicsDevice, HUDTextureBare.Width, HUDTextureBare.Height, true, SurfaceFormat.Color, DepthFormat.Depth24);
+        }
+
+        /// <summary>
+        /// Renders the health bar texture in the HUDBuffer render target
+        /// </summary>
+        public override void saveTexture(SpriteBatch spriteBatch, int health)
+        {
+            GraphicsDevice gd = Graphics.GraphicsDevice;
+            MouseState ms = Mouse.GetState();
+            gd.SetRenderTarget(HUDBuffer);
+
+            spriteBatch.Begin(SpriteSortMode.Immediate, BlendState.Opaque);
+            spriteBatch.Draw(HUDTextureFull, new Vector2(0, ScreenManager.pixelsY(0)), Color.White);
+            spriteBatch.End();
+
+            spriteBatch.Begin(SpriteSortMode.Immediate, BlendState.AlphaBlend);
+            BlendState b = new BlendState();
+            spriteBatch.GraphicsDevice.BlendState = b;
+
+            int healthLimit = ScreenManager.pixelsX(0) + HUDTextureBare.Width * health / GoodVibe.MAX_SHIELD;
+            spriteBatch.Draw(HUDAlpha, new Vector2(healthLimit, ScreenManager.pixelsY(0)), Color.White);
+            spriteBatch.End();      
+            gd.SetRenderTarget(null);
+        }
+
+        /// <summary>
+        /// Draws the health bar for the player on screen
+        /// </summary>
+        public override void draw(SpriteBatch spriteBatch)
+        {
+            Rectangle destination = new Rectangle(ScreenManager.pixelsX(30), ScreenManager.pixelsY(10), ScreenManager.pixelsX(HUDTextureBare.Width), ScreenManager.pixelsY(HUDTextureBare.Height));
+            Rectangle source = new Rectangle(0, 0, HUDTextureBare.Width, HUDTextureBare.Height);
+            spriteBatch.Draw(HUDTextureBare, destination, source, Color.White);
+            spriteBatch.Draw((Texture2D)HUDBuffer, destination, source, Color.White);
+        }
+    }
+}
