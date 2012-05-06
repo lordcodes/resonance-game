acb20194ef1de2910d8552dadbccd9108804c5fe
tompico1024@googlemail.com <tompico1024@googlemail.com@746476e4-9158-5aec-6506-eda85f564993>
2012-05-06 13:36:34 +0000

    ObjectiveManager skeleton added.

---
+++ trunk/Resonance/Resonance/Resonance/Managers/ObjectiveManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Resonance {
+    class ObjectiveManager {
+
+        public const int KILL_ALL_BV = 0;
+
+        private static int cObj = KILL_ALL_BV; // Default objective KILL_ALL_BV
+
+        public static void setObjective(int newObj) {
+            cObj = newObj;
+        }
+
+        public bool objectiveMet() {
+            return false;
+        }
+    }
+}
