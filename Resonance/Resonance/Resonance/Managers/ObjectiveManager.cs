88e22f618c657fe4f2df778dbd14a130f4a475fc
tompico1024@googlemail.com <tompico1024@googlemail.com@746476e4-9158-5aec-6506-eda85f564993>
2012-05-06 14:37:56 +0000

    ObjectiveManager added to.

--- Resonance/Resonance/Resonance/Managers/ObjectiveManager.cs
+++ Resonance/Resonance/Resonance/Managers/ObjectiveManager.cs
@@ -6,16 +6,86 @@ using System.Text;
 namespace Resonance {
     class ObjectiveManager {
 
-        public const int KILL_ALL_BV = 0;
+        public const int KILL_ALL_BV            = 0;
+        public const int COLLECT_ALL_PICKUPS    = 1;
+        public const int KILL_BOSS              = 2;
+        public const int TERRITORIES            = 3;
+        public const int SURVIVE                = 4;
 
-        private static int cObj = KILL_ALL_BV; // Default objective KILL_ALL_BV
+        private const int DEFAULT_OBJECTIVE = KILL_ALL_BV;
+
+        private static int cObj = DEFAULT_OBJECTIVE;
 
         public static void setObjective(int newObj) {
             cObj = newObj;
         }
 
+        public static int currentObjective() {
+            return cObj;
+        }
+
         public bool objectiveMet() {
             return false;
+        }
+
+        public String getObjectiveString() {
+            switch (cObj) {
+                case (KILL_ALL_BV) : {
+                    return "Destroy the Bad Vibes.";
+                }
+                case (COLLECT_ALL_PICKUPS) : {
+                    return "Collect the Pickups";
+                }
+                case (KILL_BOSS) : {
+                    return "Destroy the Master Bad Vibe";
+                }
+                case (SURVIVE) : {
+                    return "Stay alive.";
+                }
+                case (TERRITORIES) : {
+                    return "Heal each brain area by destroying the Bad Vibes therein.";
+                }
+            }
+
+            return "";
+        }
+
+        public String getProgressString() {
+            switch (cObj) {
+                case (KILL_ALL_BV) : {
+                    int killed = 0;
+                    int total = 0;
+                    return killed.ToString() + " / " + total.ToString() + " destroyed.";
+                }
+                case (COLLECT_ALL_PICKUPS) : {
+                    int collected = 0;
+                    int total = 0;
+                    return collected.ToString() + " / " + total.ToString() + " collected.";
+                }
+                case (KILL_BOSS) : {
+                    int bossHealth = 0;
+                    int bossMaxHealth = 0;
+                    double pct = 100d * (double) bossHealth / (double) bossMaxHealth;
+                    return "Master Bad Vibe " + ((int) pct).ToString() + " destroyed.";
+                }
+                case (SURVIVE) : {
+                    int remainingMins = 0;
+                    int remainingSecs = 0;
+
+                    String secString = remainingSecs.ToString();
+                    if (secString.Length < 2) secString = "0" + secString;
+                    String minString = remainingMins.ToString();
+
+                    return "Stay alive for " + minString + ":" + secString;
+                }
+                case (TERRITORIES) : {
+                    int healed = 0;
+                    int total = 0;
+                    return healed.ToString() + " / " + total.ToString() + " areas healed.";
+                }
+            }
+
+            return "";
         }
     }
 }
