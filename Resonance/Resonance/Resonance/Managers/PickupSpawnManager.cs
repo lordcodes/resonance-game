b96d094d77c5f26e2e7ad30a78c5213693b3d4b9
alexsheppard11@gmail.com <alexsheppard11@gmail.com@746476e4-9158-5aec-6506-eda85f564993>
2012-05-14 15:04:19 +0000

    Added model detail for Pickup deflect

--- Resonance/Resonance/Resonance/Managers/PickupSpawnManager.cs
+++ Resonance/Resonance/Resonance/Managers/PickupSpawnManager.cs
@@ -40,7 +40,7 @@ namespace Resonance
             pickupPool = new List<Pickup>(40);
             for (int i = 0; i < 40; i++)
             {
-                pickupPool.Add(new Pickup(GameModels.PICKUP, "Pickup" + i, Vector3.Zero, 0, 0));
+                //pickupPool.Add(new Pickup(GameModels.PICKUP, "Pickup" + i, Vector3.Zero, 0, 0));
             }
         }
 
@@ -78,16 +78,16 @@ namespace Resonance
                 Vector3 gvPos = GameScreen.getGV().Body.Position;
 
                 int minX = (int)gvPos.X - DISTANCE_FROM_PLAYER;
-                if (minX < (int)-World.MAP_X) minX = (int)-World.MAP_X;
+                if (minX < (int)-World.MAP_X) minX = (int)-World.PLAYABLE_MAP_X - 15;
 
                 int maxX = (int)gvPos.X + DISTANCE_FROM_PLAYER;
-                if (maxX > (int)World.MAP_X) maxX = (int)World.MAP_X;
+                if (maxX > (int)World.MAP_X) maxX = (int)World.PLAYABLE_MAP_X - 15;
 
                 int minZ = (int)gvPos.Z - DISTANCE_FROM_PLAYER;
-                if (minZ < (int)-World.MAP_Z) minZ = (int)-World.MAP_Z;
+                if (minZ < (int)-World.MAP_Z) minZ = (int)-World.PLAYABLE_MAP_Z - 15;
 
                 int maxZ = (int)gvPos.Z + DISTANCE_FROM_PLAYER;
-                if (maxZ > (int)World.MAP_Z) maxZ = (int)World.MAP_Z;
+                if (maxZ > (int)World.MAP_Z) maxZ = (int)World.PLAYABLE_MAP_Z - 15;
 
                 int x = r.Next(minX, maxX);
                 int z = r.Next(minZ, maxZ);
@@ -96,8 +96,16 @@ namespace Resonance
 
                 if (!ScreenManager.game.World.querySpace(pos))
                 {
-                    int rand = r.Next(0, 16) % 4;
-                    int model = types[rand];
+                    int model;                    
+                    if (ObjectiveManager.currentObjective() == ObjectiveManager.KILL_BOSS)
+                    {
+                        model = Pickup.DEFLECT;
+                    }
+                    else
+                    {
+                        int rand = r.Next(0, 16) % 4;
+                        model = types[rand];
+                    }
 
                     //Pickup p = getPickup(model, pos, r.Next(MIN_PICKUP_TIME_LIVE, MAX_PICKUP_TIME_LIVE), r.Next(MIN_PICKUP_TIME_EFFECT, MAX_PICKUP_TIME_EFFECT));
                     Pickup p = new Pickup(model, "Pickup" + totalNumPickups, pos, r.Next(MIN_PICKUP_TIME_LIVE, MAX_PICKUP_TIME_LIVE), r.Next(MIN_PICKUP_TIME_EFFECT, MAX_PICKUP_TIME_EFFECT));
