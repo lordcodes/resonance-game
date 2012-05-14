fba89d8c32c074a1a029a8af49e934f87de67108
phyros <phyros@746476e4-9158-5aec-6506-eda85f564993>
2012-05-14 18:19:07 +0000

    Set Xbox graphics for reflection, fix transparency

--- Resonance/Resonance/Resonance/Drawing/Graphics/GraphicsSettings.cs
+++ Resonance/Resonance/Resonance/Drawing/Graphics/GraphicsSettings.cs
@@ -9,29 +9,24 @@ namespace Resonance
         /// Reflections appear on the floor if true
         /// </summary>
 #if XBOX
-        public static bool FLOOR_REFLECTIONS = false;
+        public static bool FLOOR_REFLECTIONS = true;
+        public static int REFLECTION_TEXTURE_SIZE = 2048;
 #else
         public static bool FLOOR_REFLECTIONS = true;
+        public static int REFLECTION_TEXTURE_SIZE = 2048;
 #endif
 
         /// <summary>
         /// Shadows appear on the floor if true
         /// </summary>
 #if XBOX
+        // Dont change for your pc
         public static bool FLOOR_SHADOWS = false;
 #else
-        public static bool FLOOR_SHADOWS = true;
+        public static bool FLOOR_SHADOWS = false;
 #endif
 
-        /// <summary>
-        /// Resolution of the reflection texture of the floor reflections.
-        /// </summary>
-        public static int REFLECTION_TEXTURE_SIZE = 2048;
-
-        /// <summary>
-        /// Resolution of the shadows texture of the shadows.
-        /// </summary>
-        public static int SHADOWS_TEXTURE_SIZE = 2048;
+        public static int SHADOWS_TEXTURE_SIZE = 1024;
 
         /// <summary>
         /// Distance from the camera to the clip plane.
