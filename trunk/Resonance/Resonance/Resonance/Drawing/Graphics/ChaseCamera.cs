f3b2c32dc939d94e6f52063e6355c8f12e839146
alord90 <alord90@746476e4-9158-5aec-6506-eda85f564993>
2012-05-16 01:12:01 +0000

    Camera changes!

--- trunk/Resonance/Resonance/Resonance/Drawing/Graphics/ChaseCamera.cs
+++ trunk/Resonance/Resonance/Resonance/Drawing/Graphics/ChaseCamera.cs
@@ -45,44 +45,17 @@ namespace Resonance
             float z = cameraPos.Z;
 
             bool locked = false;
-            if (x + 7 >= World.PLAYABLE_MAP_X/2 || x - 7 <= -World.PLAYABLE_MAP_X/2)
+            if (x + 2 >= World.PLAYABLE_MAP_X/2 || x - 2 <= -World.PLAYABLE_MAP_X/2)
             {
                 cameraPos.X = this.position.X;
                 locked = true;
             }
-            else if (z + 7 >= World.PLAYABLE_MAP_Z/2 || z - 7 <= -World.PLAYABLE_MAP_Z/2)
+            else if (z + 2 >= World.PLAYABLE_MAP_Z/2 || z - 2 <= -World.PLAYABLE_MAP_Z/2)
             {
                 cameraPos.Z = this.position.Z;
                 locked = true;
             }
             if (locked) return cameraPos;
-
-            Vector3 rayStart = chaseObject.Position;
-            rayStart.Y = 3f;
-
-            Vector3 position;
-            RayCastResult result;
-            if (chaseObject.Space.RayCast(new Ray(rayStart, cameraPos), Vector3.Distance(rayStart,cameraPos), RayCastFilter, out result))
-            {
-                //position = result.HitData.Location - (0.01f * cameraPos);
-                position = cameraPos;
-                DebugDisplay.update("TestCast", "Can see something");
-            }
-            else
-            {
-                position = cameraPos;
-                DebugDisplay.update("TestCast", "Can't see anything");
-            }
-
-            /*List<Object> results = ScreenManager.game.World.rayCastObjects(rayStart, cameraPos, 50f, RayCastFilter);
-            if (results.Count > 0)
-            {
-                DebugDisplay.update("TestCast", results[0].returnIdentifier());
-            }
-            else
-            {
-                DebugDisplay.update("TestCast", "Can't see anything");
-            }*/
 
             return position;
         }
